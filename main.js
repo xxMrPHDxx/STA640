1615732d571cf1e80c17e2c9be80c95840f41e7c
xxMrPHDxx <pian.minecraft1@gmail.com>
2018-10-01 21:14:18 +0800

    Added Latin Square Design!

--- main.js
+++ main.js
@@ -131,6 +131,102 @@ function findANOVA_RCBD(){
 	anova.appendTo(output);
 }
 
+function findANOVA_LSD(){
+	let rawMatrix = table.getMatrixRaw();
+	let valueMatrix = new Matrix(rawMatrix.row,rawMatrix.col);
+
+	let treatments = {};
+	let nTreatment = 0;
+	let nRow = valueMatrix.row;
+	let nCol = valueMatrix.col;
+	let nTotal = nRow * nCol - 1;
+
+	rawMatrix.map((value,row,col)=>{
+		const v = value.split("=");
+		let t = v[0];
+		let val = parseFloat(v[1]);
+		if(!t) return;
+		if(typeof treatments[t] === "undefined") {
+			treatments[t] = [];
+			nTreatment++;
+		}
+		if(!isNaN(val)) valueMatrix.cell[row][col] = val;
+		treatments[t].push(valueMatrix.cell[row][col]);
+	});
+
+	let nError = nTotal - nTreatment - nRow - nCol;
+
+	if(nTreatment !== nRow || nTreatment !== nCol || nRow !== nCol) {
+		alert(`The Latin Square Design can't be apply to the data! Rows: ${nRow} != Column: ${nCol} != Treatment: ${nTreatment}`);
+		return;
+	}
+
+	const ydotdot = valueMatrix.reduce((a,b)=>a+b,0);
+	const N = nRow * nCol;
+	const ydotdot2 = Math.pow(ydotdot,2);
+
+	const ysquare = valueMatrix.reduce((a,b)=>a+Math.pow(b,2),0);
+	const SST = ysquare - ydotdot2 / N;
+
+	let sumTreatments = {};
+	for(let treatment in treatments){
+		if(!sumTreatments[treatment]) sumTreatments[treatment] = 0;
+		if(!(treatments[treatment] instanceof Array)) break;
+		for(let data of treatments[treatment]){
+			sumTreatments[treatment] += data;
+		}
+	}
+
+	let str2 = 0;
+	for(let treatment in sumTreatments){
+		str2 += Math.pow(sumTreatments[treatment],2);
+		console.log(`str2 = ${str2}, SS<${treatment}> = ${sumTreatments[treatment]}`);
+	}
+	const SSTR = str2 / nTreatment - ydotdot2 / N;
+
+	let srow2 = valueMatrix.reduceColumn((a,b)=>a+b,0).reduce((a,b)=>a+Math.pow(b,2),0);
+	const SSR = srow2 / nRow - ydotdot2 / N;
+
+	let scol2 = valueMatrix.reduceRow((a,b)=>a+b,0).reduce((a,b)=>a+Math.pow(b,2),0);
+	const SSC = scol2 / nCol - ydotdot2 / N;
+
+	const SSE = SST - SSTR - SSR - SSC;
+	const dfTR = nTreatment - 1;
+	const dfR = nRow - 1;
+	const dfC = nCol - 1;
+	const dfE = nRow * nCol - 3 * nRow + 2;
+	const dfT = nRow * nCol - 1;
+
+	let prefs = [
+		{
+			ss: SSTR,
+			df: dfTR,
+			ms: roundOff(SSTR / dfTR,3)
+		},{
+			ss: SSR,
+			df: dfR,
+			ms: roundOff(SSR / dfR,3)
+		},{
+			ss: SSC,
+			df: dfC,
+			ms: roundOff(SSC / dfC,3)
+		},{
+			ss: SSE,
+			df: dfE,
+			ms: roundOff(SSE / dfE,3)
+		},{
+			ss: SST,
+			df: dfT,
+			ms: null
+		}
+	];
+
+	let anova = new AnovaTable(prefs);
+	anova.fstat = roundOff(prefs[0].ms / prefs[3].ms,3);
+	anova.addSource("Treatment","Row","Column","Error","Total");
+	anova.appendTo(output);
+}
+
 function roundOff(value,nearest=0){
 	if(nearest < 0) nearest = 0;
 	const n = Math.pow(10,nearest);
--- table.js
+++ table.js
@@ -1,14 +1,18 @@
 class CRDTable {
 	constructor(rows=0,cols=0){
-		this.treatments = 2;
-		this.blocks = 2;
+		this.treatments = 4;
+		this.blocks = 4;
+		// this.treatments = 2;
+		// this.blocks = 2;
 		this.head = [
 			["Treatment"],
-			["X1","X2"]
+			["X1","X2","X3","X4"]
 		];
 		this.body = [
 			["Block","Y1"],
-			["Y2"]
+			["Y2"],
+			["Y3"],
+			["Y4"]
 		];
 		this.data = [];
 	}
@@ -23,6 +27,15 @@ class CRDTable {
 	appendTo(parent){
 		this.data = [];
 		let elem = document.createElement("table");
+
+
+		let initialData = [
+			["A=85","B=79","C=63","D=97"],
+			["B=91","C=81","D=80","A=93"],
+			["C=59","D=70","A=79","B=80"],
+			["D=75","A=91","B=75","C=68"]
+		]
+
 
 		let thead = document.createElement("thead");
 		for(let row=0;row<this.head.length;row++){
@@ -70,6 +83,11 @@ class CRDTable {
 				td.setAttribute("class","DataCell");
 				let input = document.createElement("input");
 				input.setAttribute("class","DataCell");
+
+				/////////////////////////
+				input.value = initialData[row][col];
+				/////////////////////////
+
 				td.appendChild(input);
 				tr.appendChild(td);
 
@@ -92,5 +110,12 @@ class CRDTable {
 			if(!isNaN(value)) data.cell[row][col] = value;
 		});
 		return typeof data === "undefined" ? new Matrix() : data;
+	}
+	getMatrixRaw(){
+		let data = new Matrix(this.blocks,this.treatments);
+		table.data.forEach(({elem,row,col})=>{
+			data.cell[row][col] = elem.value;
+		});
+		return data;
 	}
 }
